46a41b2536a31d0ab779aff1bec3351fb4f36f6b
Lew Davidson <lewdavidson47@gmail.com>
2017-11-03 08:55:59 -0700

    add pizza prototype and id's to possible toppings and sizes

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,16 @@ function Pizza(size, topping, cost) {
   this.cost = cost;
 };
 
+Pizza.prototype.pizzaTotal = function() {
+
+}
 
 var newPizza = new Pizza();
+var lgPizza = 18;
+var medPizza = 16;
+var smPizza = 12;
+var peppArt = 3;
+var mushroom = 1;
 
 
 
@@ -15,5 +23,8 @@ var newPizza = new Pizza();
 $(document).ready(function(){
   $("form#pizza-form").submit(function(event){
     event.preventDefault();
+    var pizzaSize = $("option#size-select").val();
+    var pizzaTop = $("option#topping-select").val(); 
+
   });
 });
